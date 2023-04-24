f334a7de26d7157807ea22b351260fc98d408bbc
Monika <monika.chitimalla@gmail.com>
2023-04-24 19:30:38 +0100

    Route53Changes

--- RoboshopIac/RoboshopAutomation.tf
+++ RoboshopIac/RoboshopAutomation.tf
@@ -11,6 +11,13 @@ resource "aws_instance" "Frontend" {
     Name = "Frontend"
   }
 }
+resource "aws_route53_record" "frontend" {
+  name    = "frontend.eroboshop.online"
+  type    = "A"
+  zone_id = "Z02749461DPP5346XTQCJ"
+  ttl = 30
+  records =[aws_instance.Frontend.private_ip]
+}
 resource "aws_instance" "Mongodb" {
   ami           = data.aws_ami.centosimage.image_id
   instance_type = "t3.micro"
@@ -18,6 +25,13 @@ resource "aws_instance" "Mongodb" {
   tags = {
     Name = "Mongodb"
   }
+}
+resource "aws_route53_record" "mongodb" {
+  name    = "mongodb.eroboshop.online"
+  type    = "A"
+  zone_id = "Z02749461DPP5346XTQCJ"
+  ttl = 30
+  records =[aws_instance.Mongodb.private_ip]
 }
 resource "aws_instance" "Catalogue" {
   ami           = data.aws_ami.centosimage.image_id
@@ -27,6 +41,13 @@ resource "aws_instance" "Catalogue" {
     Name = "Catalogue"
   }
 }
+resource "aws_route53_record" "catalogue" {
+  name    = "catalogue.eroboshop.online"
+  type    = "A"
+  zone_id = "Z02749461DPP5346XTQCJ"
+  ttl = 30
+  records =[aws_instance.Catalogue.private_ip]
+}
 resource "aws_instance" "Cart" {
   ami           = data.aws_ami.centosimage.image_id
   instance_type = "t3.micro"
@@ -34,6 +55,13 @@ resource "aws_instance" "Cart" {
   tags = {
     Name = "Cart"
   }
+}
+resource "aws_route53_record" "Cart" {
+  name    = "cart.eroboshop.online"
+  type    = "A"
+  zone_id = "Z02749461DPP5346XTQCJ"
+  ttl = 30
+  records =[aws_instance.Cart.private_ip]
 }
 resource "aws_instance" "User" {
   ami           = data.aws_ami.centosimage.image_id
@@ -43,6 +71,13 @@ resource "aws_instance" "User" {
     Name = "User"
   }
 }
+resource "aws_route53_record" "User" {
+  name    = "user.eroboshop.online"
+  type    = "A"
+  zone_id = "Z02749461DPP5346XTQCJ"
+  ttl = 30
+  records =[aws_instance.User.private_ip]
+}
 resource "aws_instance" "MySql" {
   ami           = data.aws_ami.centosimage.image_id
   instance_type = "t3.micro"
@@ -50,6 +85,13 @@ resource "aws_instance" "MySql" {
   tags = {
     Name = "MySql"
   }
+}
+resource "aws_route53_record" "Mysql" {
+  name    = "mysql.eroboshop.online"
+  type    = "A"
+  zone_id = "Z02749461DPP5346XTQCJ"
+  ttl = 30
+  records =[aws_instance.MySql.private_ip]
 }
 resource "aws_instance" "Redis" {
   ami           = data.aws_ami.centosimage.image_id
@@ -59,6 +101,13 @@ resource "aws_instance" "Redis" {
     Name = "Redis"
   }
 }
+resource "aws_route53_record" "Redis" {
+  name    = "redis.eroboshop.online"
+  type    = "A"
+  zone_id = "Z02749461DPP5346XTQCJ"
+  ttl = 30
+  records =[aws_instance.Redis.private_ip]
+}
 resource "aws_instance" "RabbitMQ" {
   ami           = data.aws_ami.centosimage.image_id
   instance_type = "t3.micro"
@@ -66,6 +115,13 @@ resource "aws_instance" "RabbitMQ" {
   tags = {
     Name = "RabbitMQ"
   }
+}
+resource "aws_route53_record" "RabbitMQ" {
+  name    = "rabbitmq.eroboshop.online"
+  type    = "A"
+  zone_id = "Z02749461DPP5346XTQCJ"
+  ttl = 30
+  records =[aws_instance.RabbitMQ.private_ip]
 }
 resource "aws_instance" "Payment" {
   ami           = data.aws_ami.centosimage.image_id
@@ -75,6 +131,13 @@ resource "aws_instance" "Payment" {
     Name = "Payment"
   }
 }
+resource "aws_route53_record" "Payment" {
+  name    = "payment.eroboshop.online"
+  type    = "A"
+  zone_id = "Z02749461DPP5346XTQCJ"
+  ttl = 30
+  records =[aws_instance.Payment.private_ip]
+}
 resource "aws_instance" "Shipping" {
   ami           = data.aws_ami.centosimage.image_id
   instance_type = "t3.micro"
@@ -83,6 +146,13 @@ resource "aws_instance" "Shipping" {
     Name = "Shipping"
   }
 }
+resource "aws_route53_record" "Shipping" {
+  name    = "shipping.eroboshop.online"
+  type    = "A"
+  zone_id = "Z02749461DPP5346XTQCJ"
+  ttl = 30
+  records =[aws_instance.Shipping.private_ip]
+}
 resource "aws_instance" "Dispatch" {
   ami           = data.aws_ami.centosimage.image_id
   instance_type = "t3.micro"
@@ -90,4 +160,11 @@ resource "aws_instance" "Dispatch" {
   tags = {
     Name = "Dispatch"
   }
+}
+resource "aws_route53_record" "Dispatch" {
+  name    = "dispatch.eroboshop.online"
+  type    = "A"
+  zone_id = "Z02749461DPP5346XTQCJ"
+  ttl = 30
+  records =[aws_instance.Dispatch.private_ip]
 }

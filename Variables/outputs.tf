1b510ce0af17eeb7c648438000f031485972cf73
Monika <monika.chitimalla@gmail.com>
2023-05-02 18:49:51 +0100

    Basic types of Variables in Terraform

---
+++ Variables/outputs.tf
@@ -0,0 +1,24 @@
+output "sample_string" {
+  value = var.sample_string
+}
+
+## In above case we are printing just variable, If variable is a combination of some strings then following is the syntax. ${} is mandatory if we include the variable inside a string
+
+output "sample_string1" {
+  value = "Value of sample_string = ${var.sample_string}"
+}
+
+## Values from list
+# Second value in list
+output "sample_list_2" {
+  value = var.sample_list[1]
+}
+
+# Print number1 in sample_dict
+output "sample_dict_num" {
+  value = var.sample_dict["number"]
+}
+
+output "sample_dict_bool" {
+  value = var.sample_dict["boolean"]
+}
---
+++ Variables/vars.tf
@@ -0,0 +1,25 @@
+variable "sample_string" {
+  default = "Hello World"
+}
+variable "sample_number" {
+  default = 100
+}
+variable "sample_boolean" {
+  default = true
+}
+#Indexing starts from 0. First element is var.sample_list[0]
+variable "sample_list" {
+  default = [
+    100,
+    "Apple",
+    true,
+    10.6
+  ]
+}
+variable "sample_dict" {
+  default={
+    number=12
+    string="Sample String",
+    boolean=false
+  }
+}

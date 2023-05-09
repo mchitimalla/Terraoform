c1acd01a46c66cb250c842c91d0ca24acff48c77
Monika <monika.chitimalla@gmail.com>
2023-05-09 10:10:02 +0100

    Basic types of Variables in Terraform

---
+++ Loops/ForLoop.tf
@@ -0,0 +1,39 @@
+variable "Fruits1" {
+  default = {
+    fruit1={
+    name="apple"
+    count=10
+    }
+    fruit2={
+      name="banana"
+      count=20
+    }
+    fruit3={
+      name="mango"
+      count=30
+    }
+  }
+}
+resource "null_resource" "PrintFruits1" {
+  for_each = var.Fruits1
+  provisioner "local-exec" {
+    command = "echo ${each.key} - ${each.value["name"]},${each.value["count"]} "
+  }
+}
+variable "List" {
+  pencils=10
+  eraser=20
+  markers=12
+}
+resource "null_resource" "PrintList" {
+  for_each = var.List
+  provisioner "local-exec" {
+    command = "echo ${each.key} - ${each.value}} "
+  }
+}
+resource "null_resource" "PrintForFruits" {
+  for_each = var.Fruits
+  provisioner "local-exec" {
+    command = "echo ${each.key} "
+  }
+}

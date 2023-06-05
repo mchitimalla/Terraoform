fe65ec60a444d4e347dc808c99919998acf7b2fc
Monika <monika.chitimalla@gmail.com>
2023-06-05 14:07:00 +0100

    Learning

---
+++ AWS_SpotInstance/main.tf
@@ -0,0 +1,17 @@
+data "aws_ami" "centosimage"{
+  owners = ["973714476881"]
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+}
+data "aws_security_group" "AllowAll"{
+  name="MyRoboGroupAllowAll"
+}
+resource "aws_spot_instance_request" "instance" {
+  ami           = data.aws_ami.centosimage.image_id
+  instance_type = "t3.micro"
+  instance_interruption_behaviour = "stop"
+  vpc_security_group_ids = data.aws_security_group.AllowAll.id
+  tags = {
+    Name="spot Instance"
+  }
+}
